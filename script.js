88a81ce8baebbe07fc39f2591edba9030b318620
e-basoglu <151559231+e-basoglu@users.noreply.github.com151559231+e-basoglu@users.noreply.github.com>
2024-05-03 09:59:00 +0200

    Task 1 completed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // Set this constant to true to debug the placement of bombs without
 // having to click on all cells to reveal them.
-const CHEAT_REVEAL_ALL = false;
+const CHEAT_REVEAL_ALL = true;
 
 const ROWS_COUNT = 10;
 const COLS_COUNT = 10;
@@ -27,11 +27,28 @@ for (var row = 0; row < ROWS_COUNT; row++) {
 
 //
 // TODO: Task 1 - add some bombs at fixed positions.
-// cells[0][1].isBomb = true;
-// cells[5][4].isBomb = true;
-// cells[9][9].isBomb = true;
+function definedBomb() {
+  cells[0][0].isBomb = true;
+  cells[1][7].isBomb = true;
+  cells[4][9].isBomb = true;
+  cells[8][2].isBomb = true;
+  cells[9][2].isBomb = true;
+  cells[9][9].isBomb = true;
+}
 
-//
+// call definedBomb function
+definedBomb();
+
+
+// add some bombs at fixed positions.
+
+
+
+
+
+
+
+
 // TODO: Task 2 - Comment out the code of task 1. Instead of adding bombs in fixed places, add 10 of them in random places.
 //                Add a BOMBS_COUNT constant so that you can easily change the amount of bombs placed. Put it next to the
 //                other constants.
